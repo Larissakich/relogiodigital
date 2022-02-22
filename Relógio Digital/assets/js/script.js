bfcfe927f58609683277fa1e8fb4ca7dead7be0f
Larissa Kich <76120366+Larissakich@users.noreply.github.com>
2022-02-22 20:57:42 -0300

    Add files via upload

---
+++ Relógio Digital/assets/js/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+})
